19f28311bf532b9dc30bb726fa0d2d071319813e
Maria <mary.chemikos@gmail.com>
2026-03-07 16:27:24 +0200

    fix: rename HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-const Catalog = () => {
+const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-blue-500">Home</h1>
@@ -6,4 +6,4 @@ const Catalog = () => {
   );
 };
 
-export default Catalog;
+export default HomePage;
