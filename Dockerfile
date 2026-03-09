4757c881e3e658dc4864220902b92f5b6ba06acb
Hapendo <utgirovec@gmail.com>
2026-03-09 21:05:29 +0200

    React integration

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# React
+FROM node:20-alpine AS frontend-build
+WORKDIR /app
+COPY frontend/package*.json ./
+RUN npm ci
+COPY frontend/ .
+RUN npm run build
+
+# Backend
+FROM php:8.2-fpm
+WORKDIR /var/www
+
+# PHP
+RUN apt-get update && apt-get install -y \
+    libzip-dev zip unzip git curl libpq-dev libonig-dev libxml2-dev pkg-config \
+    && docker-php-ext-install pdo pdo_mysql pdo_pgsql mbstring xml ctype bcmath
+
+# Composer
+COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
+
+# Copy Laravel
+COPY backend/ .
+
+# Dependencies
+RUN composer install --no-dev --optimize-autoloader --prefer-dist --no-interaction
+
+# Copy React build to public
+COPY --from=frontend-build /app/dist ./public
+
+# Rights
+RUN chown -R www-data:www-data /var/www
+
+# Run Laravel + migrations
+ENV PORT 10000
+CMD php artisan migrate --force && php artisan serve --host=0.0.0.0 --port=$PORT
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router';
 import HomePage from './pages/HomePage';
 import Layout from './Layout';
 
 function App() {
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    async function checkBackend() {
+      try {
+        const res = await fetch(`${API_URL}/health`);
+        const data = await res.json();
+        console.log("Backend status:", data); // {status: 'ok'}
+      } catch (err) {
+        console.error("Backend error:", err);
+      }
+    }
+
+    checkBackend();
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}></Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
